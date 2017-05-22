cedccd5134eda4b39fd3b362b9ddf05e8a4d64d0
Slavik <hubenya@hotmail.com>
2017-05-22 14:29:25 -0700

    i forgot to checkout new branch

---
+++ app.js
@@ -0,0 +1,5 @@
+//global variables
+//var totalcounter = 0;
+var images = ['bag', 'banana', 'bathroom', 'boots', 'breafast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+console.log('image names', images[0]);
