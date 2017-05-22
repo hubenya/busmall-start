1630ae760535f03a8d3dc3499432ea95f3bebd76
Slavik <hubenya@hotmail.com>
2017-05-22 15:45:39 -0700

    added html markup

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
 //global variables
-//var totalcounter = 0;
+var totalcounter = 0;
 var images = ['bag', 'banana', 'bathroom', 'boots', 'breafast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-console.log('image names', images[0]);
+//console.log('image names', images[0]);
+//construction function
+// function ImageConstructor(){
+//   this.images = images;
+//   this.path = name + .jpg;
+//   this.click = 0;
+// }
+
+
+// function ImageTracker(name, paths, shown, clicked)
+//   this.name = name;
+//   this.path = path;
+//   this.shown = 0;
+//   this.cliked = 0;
+// }
+var counter = 1;
+var image = document.getElementById('image1');
+
+image.addEventListener('click', function() {
+  console.log('my image type:', this.getAttribute('src'));
+  counter++;
+
+  console.log('counter', counter);
+});
+// 
+// var image = document.getElementById('image2');
+//
+// image.addEventListener('click', function() {
+//   console.log('my image type:', this.getAttribute('src'));
+//   counter++;
+// });
+//
+// var image = document.getElementById('image3');
+//
+// image.addEventListener('click', function() {
+//   console.log('my image type:', this.getAttribute('src'));
+//   counter++;
+//
+//   console.log('counter', counter);
+// });
