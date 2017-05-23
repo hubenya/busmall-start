87e62e2c7f4685889a0968f448a516b7960a089c
Slavik <hubenya@hotmail.com>
2017-05-23 14:34:23 -0700

    added my java script

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-
 //global variables
 var imageArray = [];
-//var usedImages = [];
-//var totalCounter = 0;
+var imagesLastShown = [];
+var displayedImages = [];
+var totalCounter = 0;
+var list = document.getElementById('list')
 
 //construction function
 function imageCreator(name, filePath){
   this.name = name;
   this.filePath = filePath;
   this.imageViews = 0;
-  this.click = 0;
+  this.clicks = 0;
   imageArray.push(this);
 }
 
@@ -34,57 +35,69 @@ var wineGlass = new imageCreator('wine-glass', './img/wine-glass.jpg');
 var sweep = new imageCreator('sweep', './img/sweep.png');
 var usb = new imageCreator('usb', './img/usb.gif');
 
-var imageList = [bag, banana, bathroom, boots, breafast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, tauntaun, unicorn, watercan, wineGlass, sweep, usb];
-console.log('image names', imageList);
+// var imageList = [bag, banana, bathroom, boots, breafast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, tauntaun, unicorn, watercan, wineGlass, sweep, usb];
+// console.log('image names', imageList);
+
 
 //i'm suppostu generate random image and get that added to my whlie loop
 function randomImage (){
-  var randomNumber = Math.floor(Math.random() * imageList.length);
+  for (var i = 0; i < 3; i++);
+  displayedImages.push(imageArray[Math.floor(Math.random() * imageArray.length)]);
     //console.log('random number generator', randomNumber);
-  return imageArray[randomNumber];
 }
-randomImage();
-console.log(randomImage());
+
+randomImage(imageArray.length);
 
 //function to render randomImages to the page after user clicks on them.
+function myClicker(click) {
+  for (var i = 0; i < imageArray.length; i++) {
+    if (imageArray[i].name === click) {
+      imageArray[i].clicks ++;
+      console.log(imageArray[i].clicks);
+    }
+  }
+}
 
 function render() {
-  for (k = 0; k < 3; k++){
-    var display = document.getElementById('image');
-    var object = displayedImages[k];
+  for (var k = 0; k < 3; k++){
+    var myImage = document.getElementById('myImage');
+    var myObj = displayedImages[k];
+    var image = document.createElement('img');//neet to fix this
     image = document.createElement('img');
     image.setAttribute('class','survey');
-    image.setAttribute('src', object.path);
-    image.setAttribute('id', object.name);
-    display.appendChild(image);
-    object.displayCount++;
-    image.addEventListiner('click', eventHandler);
-
+    image.setAttribute('src', myObj.filePath);
+    image.setAttribute('id', myObj.name);
+    image.addEventListener('click', eventHandler);
+    myImage.appendChild(image);
+    myObj.imageViews++;
   }
 }
 
 //event handler function not connected at the moment but will soon
 function eventHandler(event) {
-  if (counter < 25) {
+  if (totalCounter < 24) {
     var selected = event.target;
-    clicker(selected.id); {
-      for (var i=0; i < imageList.length; i++); {
-        if (imageList[i] === selected.id); {
-          imageList[i].clickCount++;
-        }
+    console.log(selected);
+    console.log(selected.id);
+    myClicker(selected.id); {
+      totalCounter ++;
+      imagesLastShown = displayedImages;
+      displayedImages = [];
+      document.getElementById('myImage').innerHTML = '';
+      randomImage(imageArray.length);
+      render();
+      console.log(selected.clicks);
+    } else {
+      document.getElementById('myImage').innerHTML = '';
+      var ul = document.createElement('ul');
+      document.body.appendChild(ul);
+      for (var d = 0; d < imageArray.length; d++) {
+        var li = document.createElement('li');
+        li.innerHTML = imageArray[d].name + ' cliked by user ' + imageArray[d].cliks + ' times and clicked ' + Math.floor(imageArray[d].clicks/imageArray[d].displayCount * 100) + ' precent of times displayed';
+        li
       }
     }
-    lastShown = displayedImages;
-    displayedImages = [];
-    document.getElementById('image1').innerHTML = '';
-    randomImage(imageList.length);
-    render();
-  } else {
-
-
   }
 }
-
-
 
 // i need to add my while loops to loop throught the each image. i'm not there yet.
